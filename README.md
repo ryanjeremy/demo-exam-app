823c074fafd2fbbe9d6d17f62eeab3e0f9a861b6
Ryan <ryan@mindsharelabs.com>
2018-07-20 21:03:36 -0600

    initial client/server wireframe

---
+++ README.md
@@ -0,0 +1,12 @@
+# Demo Exam App
+
+This is a demo app that offers a short programming quiz to inexperienced coders. We'll be using Express.js to serve a simple API service and React.js to serve the front end application.
+
+## Getting Started
+
+To get started and install the dependancies for both the client and server, and then run the application, go ahead and execute the following two commands:
+
+```
+yarn install-dependencies
+yarn start
+```
---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	state = {
+		initialText: "Loading..."
+	};
+	
+  componentDidMount() {
+    fetch('http://localhost:5000/api/test/get')
+    .then(response => response.json())
+    .then(jsonResponse => {
+      this.setState({
+        initialText: jsonResponse.result
+      });
+    })
+    .catch(error => {
+        this.setState({
+          initialText: "Error loading initial text from API"
+        });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          {this.state.initialText}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
