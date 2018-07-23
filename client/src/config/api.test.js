1e4869a49d394fadbb7b2279aaca10a43c9b8d78
Ryan <ryan@mindsharelabs.com>
2018-07-22 20:25:31 -0600

    updated api test file

--- client/src/config/api.test.js
+++ client/src/config/api.test.js
@@ -7,12 +7,19 @@ it('API dispatches quiz', () => {
 });
 
 it ('API accurately scores responses', () => {
-  return getScore([
+  const quizAnswers = [
     {id:"array-index", type:"dropdown", response:"1"},
+    {id:"react-component-update", type:"dropdown", response:"0"},
+    {id:"test-code", type:"dropdown", response:"1"},
     {id:"math-random", type:"input", response:"4"},
-    {id:"switch-end", type:"toggle", response:false}
-  ])
-  .then(result => {
-    expect(result.score).toBe(3);
-  })
+    {id:"string-upper-case", type:"input", response:"HELLO WORLD"},
+    {id:"jsx-class", type:"input", response:"className"},
+    {id:"switch-end", type:"toggle", response:false},
+    {id:"mutable-data-types", type:"toggle", response:false},
+    {id:"visible-variable", type:"toggle", response:false}
+  ]
+  return getScore(quizAnswers)
+    .then(result => {
+      expect(result.score).toBe(quizAnswers.length);
+    })
 });
